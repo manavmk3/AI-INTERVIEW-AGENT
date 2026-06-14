d035566affe5a5fbfa47d21ade6218b70c999b7a
Manav Kumar <kumarmanav.kumar17@gmail.com>
2026-06-14 15:55:59 +0530

    Initial commit: Set up server with DB connection and User schema

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT || 8000
+
+app.get("/", (req, res) => {
+    return res.json({ message: " server started " })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
