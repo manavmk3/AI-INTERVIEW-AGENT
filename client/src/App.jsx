73931bf9b7fdb99078fa8316c54323d9ad52eed9
Manav Kumar <kumarmanav.kumar17@gmail.com>
2026-06-21 23:41:06 +0530

    Setup routing, Tailwind CSS v4, and Auth page - Installed react-router-dom, react-icons, and motion - Configured Home/Auth routes & added vite polling config - Created animated HIREX.AI Auth card component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import Auth from './pages/Auth'
 
 function App() {
   return (
-    <div>Home Page</div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/auth' element={<Auth />} />
+    </Routes>
   )
 }
 
-export default App  
+export default App
